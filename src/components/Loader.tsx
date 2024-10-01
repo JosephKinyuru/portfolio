b24c6998a6f618238c2547abc4175e97899e89fb
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-10-01 12:18:46 +0300

    Added animation variable to useEffect ( solved dependancy array bug)

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -46,47 +46,48 @@ const StyledLoader = styled.div<StyledLoaderProps>`
 const Loader = ({ finishLoading } : LoaderProps ) => {
   const [isMounted, setIsMounted] = useState(false);
 
-  const animate = () => {
-    const loader = anime.timeline({
-      complete: () => finishLoading(),
-    });
-
-    loader
-      .add({
-        targets: '#logo path',
-        delay: 300,
-        duration: 1500,
-        easing: 'easeInOutQuart',
-        strokeDashoffset: [anime.setDashoffset, 0],
-      })
-      .add({
-        targets: '#logo #B',
-        duration: 700,
-        easing: 'easeInOutQuart',
-        opacity: 1,
-      })
-      .add({
-        targets: '#logo',
-        delay: 500,
-        duration: 300,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        scale: 0.1,
-      })
-      .add({
-        targets: '.loader',
-        duration: 200,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        zIndex: -1,
-      });
-  };
-
   useEffect(() => {
+
+    const animate = () => {
+      const loader = anime.timeline({
+        complete: () => finishLoading(),
+      });
+  
+      loader
+        .add({
+          targets: '#logo path',
+          delay: 300,
+          duration: 1500,
+          easing: 'easeInOutQuart',
+          strokeDashoffset: [anime.setDashoffset, 0],
+        })
+        .add({
+          targets: '#logo #B',
+          duration: 700,
+          easing: 'easeInOutQuart',
+          opacity: 1,
+        })
+        .add({
+          targets: '#logo',
+          delay: 500,
+          duration: 300,
+          easing: 'easeInOutQuart',
+          opacity: 0,
+          scale: 0.1,
+        })
+        .add({
+          targets: '.loader',
+          duration: 200,
+          easing: 'easeInOutQuart',
+          opacity: 0,
+          zIndex: -1,
+        });
+    };
+    
     const timeout = setTimeout(() => setIsMounted(true), 10);
     animate();
     return () => clearTimeout(timeout);
-  }, []);
+  }, [finishLoading]);
 
   return (
     <StyledLoader className="loader" isMounted={isMounted}>
